0b452d8e37eac1020901d5b2e4f705376e279710
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-01-26 13:42:13 -0800

    bug fix

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -30,7 +30,7 @@ Get in touch with me on Github: [${answers.username}](https://github.com/${answe
 <br/>
 If you have any additional questions, please contact me at: ${answers.email}
 `;
-}
+} 
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -39,6 +39,7 @@ function renderLicenseLink(license) {}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
